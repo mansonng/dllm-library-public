c6aea59e6f8a0260b7b10902c716f40331081c01
rabbit878 <18241358+rabbit878@users.noreply.github.com>
2025-09-07 22:43:11 +1000

    Comments with pagination

--- functions/src/itemService.ts
+++ functions/src/itemService.ts
@@ -1,6 +1,7 @@
 import { db, GetPublicUrlForGSFile } from "./platform";
 import {
   Item,
+  ItemCommentsConnection,
   Location,
   LocationInput,
   ItemCondition,
@@ -272,6 +273,14 @@ export class ItemService {
       language: language,
       created: Timestamp.now(),
       updated: Timestamp.now(),
+      commentsConnection: {
+        comments: [],
+        pageInfo: {
+          hasNextPage: false,
+          startCursor: "dummy",
+          endCursor: "dummy",
+        },
+      },
     };
 
     // Only add optional fields if they have valid values
